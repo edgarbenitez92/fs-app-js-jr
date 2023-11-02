f3d5f0150472c36c97d99e2efb2d971667b9a5cb
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-02 14:51:22 -0300

    Incorporamos formulario Login
    -. Corregimos algunas importaciones
    -. Colocamos styles para centrado de contenido
    -. Incorporamos formulario con función para hacer login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { base_routes } from './app.routes';
+import { CoreModule } from './core/core.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(base_routes),
-    importProvidersFrom(),
+    importProvidersFrom(CoreModule),
     provideHttpClient(withInterceptorsFromDi()),
   ],
 };
--- src/app/view/todo/login/login.component.ts
+++ src/app/view/todo/login/login.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  private formBuilder = inject(FormBuilder);
+  formLogin: FormGroup<LoginForm>;
 
+  constructor() {
+    this.formLogin = this.formBuilder.nonNullable.group<LoginForm>({
+      user: new FormControl(),
+      password: new FormControl(),
+    });
+  }
+
+  doLogin(form: FormGroup) {
+    console.log('formLogin: ', form.value);
+  }
+}
+
+interface LoginForm {
+  user: FormControl<string | null>;
+  password: FormControl<number | null>;
 }
