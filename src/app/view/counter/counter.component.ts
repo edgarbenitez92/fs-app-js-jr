33881af674447c64517af09692133ce6eda2eb91
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-02 13:10:41 -0300

    Creamos función de reset de counter

--- src/app/view/counter/counter.component.ts
+++ src/app/view/counter/counter.component.ts
@@ -2,13 +2,14 @@ import { Component, inject } from '@angular/core';
 import { NumberToCounterComponent } from './number-to-counter/number-to-counter.component';
 import { NgFor } from '@angular/common';
 import { CounterService } from 'src/app/services/counter.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss'],
   standalone: true,
-  imports: [NumberToCounterComponent, NgFor],
+  imports: [NumberToCounterComponent, NgFor, MatButtonModule],
 })
 export class CounterComponent {
   numbersToCounter: number[] = [1, 3, 5];
@@ -24,5 +25,9 @@ export class CounterComponent {
     this.counterService.counter.subscribe((value) => {
       this.counter = value;
     });
+  }
+
+  resetCounter() {
+    this.counterService.counter.next(0);
   }
 }
