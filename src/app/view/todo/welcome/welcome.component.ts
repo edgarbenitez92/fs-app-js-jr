4f7dcbeb5c75b60bf0cc2f588dfc50c428ed8e2c
Edgar Benítez <edgarbenitez92@gmail.com>
2023-11-03 09:23:50 -0300

    Creación de TODOS component
    -. Adaptamos el código en WelcomeComponent para mostrar el TODOS component
    -. Cambiamos el título en el Welcome

--- src/app/view/todo/welcome/welcome.component.ts
+++ src/app/view/todo/welcome/welcome.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, UrlSegment } from '@angular/router';
+import { TodosComponent } from 'src/app/shared/todos/todos.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TodosComponent],
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
+  private activatedRoute = inject(ActivatedRoute);
+  user!: string;
 
+  ngOnInit(): void {
+    this.getUserNameByUrl();
+  }
+
+  getUserNameByUrl(): void {
+    this.activatedRoute.url.subscribe((url: UrlSegment[]) => {
+      this.user = url[1].path;
+    });
+  }
 }
