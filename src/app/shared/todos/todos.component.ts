bd120d9215e17f7d43a33d281a9b22be4557e8fb
Edgar Benítez <edgarbenitez92@gmail.com>
2023-11-03 15:54:40 -0300

    Incluímos lista en duro de TODOS en una tabla

--- src/app/shared/todos/todos.component.ts
+++ src/app/shared/todos/todos.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent {
+  currentDate: Date = new Date();
+  todos: TodoList[] = [
+    {
+      id: 1,
+      description: 'Learn AWS',
+      done: false,
+      targetDate: new Date(this.currentDate.getFullYear() + 2),
+    },
+    {
+      id: 2,
+      description: 'Learn Spring Boot Java',
+      done: false,
+      targetDate: new Date(this.currentDate.getFullYear() + 11),
+    },
+    {
+      id: 3,
+      description: 'Learn English',
+      done: false,
+      targetDate: new Date(this.currentDate.getFullYear() + 1),
+    },
+  ];
 
+  dataSourceTodo: MatTableDataSource<TodoList> =
+    new MatTableDataSource<TodoList>();
+  displayedColumnsTodos: string[] = ['id', 'description', 'done', 'targetDate'];
+
+  ngOnInit(): void {
+    this.buildDataSourceTable();
+  }
+
+  buildDataSourceTable(): void {
+    this.dataSourceTodo = new MatTableDataSource<TodoList>(this.todos);
+  }
+}
+
+interface TodoList {
+  id: number;
+  description: string;
+  done: boolean;
+  targetDate: Date;
 }
