7ee33d8b52c772121eb0909d2dadb6ad1b127148
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-06 17:54:32 -0300

    Consumo de nuevo endpoint
    -. Consumimos nuevo endpoint de delete
    -. Eliminamos columna ID y colocamos ACTIONS
    -. Styles variados
    -. Actualizamos importaciones

--- src/app/shared/todos/todos.component.ts
+++ src/app/shared/todos/todos.component.ts
@@ -5,43 +5,51 @@ import { MatIconModule } from '@angular/material/icon';
 import { HttpErrorResponse } from '@angular/common/http';
 import { TodoService } from 'src/app/core/services/todo.service';
 import { SessionService } from 'src/app/core/services/session.service';
+import { MatButtonModule } from '@angular/material/button';
+import { Todo } from 'src/app/core/interfaces/todo.interface';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatIconModule],
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent {
   private todoService = inject(TodoService);
   private sessionService = inject(SessionService);
+  user = this.sessionService.getToken();
 
   currentDate: Date = new Date();
-  dataSourceTodo: MatTableDataSource<TodoList> =
-    new MatTableDataSource<TodoList>();
-  displayedColumnsTodos: string[] = ['id', 'description', 'done', 'targetDate'];
+  dataSourceTodo: MatTableDataSource<Todo> = new MatTableDataSource<Todo>();
+  displayedColumnsTodos: string[] = [
+    'description',
+    'done',
+    'targetDate',
+    'actions',
+  ];
 
   ngOnInit(): void {
     this.getTodosByUser();
   }
 
-  buildDataSourceTable(todos: TodoList[]): void {
-    this.dataSourceTodo = new MatTableDataSource<TodoList>(todos);
+  buildDataSourceTable(todos: Todo[]): void {
+    this.dataSourceTodo = new MatTableDataSource<Todo>(todos);
   }
 
   getTodosByUser() {
-    const user = this.sessionService.getToken();
-    this.todoService.getTodosByUser(user).subscribe({
+    this.todoService.getTodosByUser(this.user).subscribe({
       next: (todos) => this.buildDataSourceTable(todos),
       error: (err: HttpErrorResponse) => console.error('Error: ', err),
     });
   }
-}
 
-interface TodoList {
-  id: number;
-  description: string;
-  done: boolean;
-  targetDate: Date;
+  deleteTodoById(event: Event, todo: Todo) {
+    event.stopPropagation();
+
+    this.todoService.deleteTodoById(todo.id, this.user).subscribe({
+      next: (todos: Todo[]) =>
+        (this.dataSourceTodo = new MatTableDataSource<Todo>(todos)),
+    });
+  }
 }
