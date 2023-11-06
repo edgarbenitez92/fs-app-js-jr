58aefbd7085c922dfdfc75bc3084ba88c4ba5eb0
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-06 17:53:50 -0300

    Inyección de servicio que permite eliminar TODO por ID
    -. Creamos todo.interface
    -. Luego de eliminar, hacemos un switchMap para actualizar la lista de TODOs

--- src/app/core/services/todo.service.ts
+++ src/app/core/services/todo.service.ts
@@ -1,18 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { environment } from 'src/app/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTodosByUser(user: string): Observable<any> {
     const headers = { 'Content-Type': 'application/json' };
-    return this.http.get(`${environment.base_url}/users/${user}/todos`, { headers });
+    return this.http.get(`${environment.base_url}/users/${user}/todos`, {
+      headers,
+    });
   }
 
+  deleteTodoById(id: number, user: string): Observable<any> {
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http
+      .delete(`${environment.base_url}/users/${user}/todos/${id}/delete-todo`, {
+        headers,
+      })
+      .pipe(switchMap(() => this.getTodosByUser(user)));
+  }
 }
