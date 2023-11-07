38dee93e2598e90e642bc19f1c8872484a6c9f89
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-06 21:39:17 -0300

    Ajustes en el navbar para que sea compartido

--- src/app/view/todo/todo.component.ts
+++ src/app/view/todo/todo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
 })
 export class TodoComponent {}
